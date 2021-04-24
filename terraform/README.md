31f722ab40362c0d82a467dc91e42060f44994a2
“novanovn” <novanovn@gmail.com>
2021-04-24 15:00:42 +0700

    test

---
+++ terraform/README.md
@@ -0,0 +1,2 @@
+# terraform-aws-vpc-ec2-docker-v1
+Example AWS Deployment Script with Terraform for VPC, EC2 &amp; Docker App
---
+++ terraform/subnets.tf
@@ -0,0 +1,34 @@
+# Resource: aws_subnet
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+resource "aws_subnet" "public_1" {
+  # The VPC ID.
+  vpc_id = aws_vpc.main.id
+  # The CIDR block for the subnet.
+  cidr_block = "192.168.0.0/18"
+  # The AZ for the subnet.
+  availability_zone = "us-east-1a"
+  # Required for EKS. Instances launched into the subnet should be assigned a public IP address.
+  map_public_ip_on_launch = true
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                        = "public-us-east-1a"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+  }
+}
+
+
+resource "aws_subnet" "private_1" {
+  # The VPC ID
+  vpc_id = aws_vpc.main.id
+  # The CIDR block for the subnet.
+  cidr_block = "192.168.128.0/18"
+  # The AZ for the subnet.
+  availability_zone = "us-east-1a"
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                              = "private-us-east-1a"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
